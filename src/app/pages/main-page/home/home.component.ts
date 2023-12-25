1f14d7fe3c2c9a1bac12e0bb40276c4bf0993da6
Kevin <Kevo daniel>
2023-12-25 14:52:37 -0500

    Listo para Revisión

--- src/app/pages/main-page/home/home.component.ts
+++ src/app/pages/main-page/home/home.component.ts
@@ -4,6 +4,7 @@ import { Icliente } from 'src/app/interface/icliente';
 import { Icompra } from 'src/app/interface/icompra';
 import { Icotizacion } from 'src/app/interface/icotizacion';
 import { Iempleados } from 'src/app/interface/iempleados';
+import { Iproducto } from 'src/app/interface/iproducto';
 import { Iproveedor } from 'src/app/interface/iproveedor';
 import { ClientesService } from 'src/app/services/clientes.service';
 import { ComprasService } from 'src/app/services/compras.service';
@@ -12,6 +13,8 @@ import { EmpleadosService } from 'src/app/services/empleados.service';
 import { ProductosService } from 'src/app/services/productos.service';
 import { ProveedoresService } from 'src/app/services/proveedores.service';
 import { VentasService } from 'src/app/services/ventas.service';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable'
 
 @Component({
   selector: 'app-home',
@@ -40,14 +43,14 @@ variables globales para definir el inventario de clientes
 ==========================================================*/
   clientes = 0;
   loadClientes = false;
-  lstClientes!:Icliente[];
+  lstClientes!: Icliente[];
 
   /*========================================================
 variables globales para definir el inventario de empleados
 ==========================================================*/
   empleados = 0;
   loadEmpleados = false;
-  lstEmpleados!:Iempleados[];
+  lstEmpleados!: Iempleados[];
 
   /*========================================================
 variables globales para definir el inventario de cotizaciones
@@ -98,10 +101,10 @@ variables globales para definir el inventario de cotizaciones
     type: "Bar", //TIPO DE GRAFICO QUE ACEPTA EL PLUGINS
     data: [],
     //PARA DATA LO QUE ESTA EN LA [0] ES X  , [1] ES Y
-    columnNames: ['Repuesto', 'Cantidad',{ role: 'annotation' }], //COMO SE LLAMAN LAS COLUMNAS X,Y
+    columnNames: ['Repuesto', 'Cantidad', { role: 'annotation' }], //COMO SE LLAMAN LAS COLUMNAS X,Y
     options: {
       colors: ['#00008F'],
-      legend: 'none' ,
+      legend: 'none',
       hAxis: {
         title: 'Cantidad Vendida'
       },
@@ -110,13 +113,13 @@ variables globales para definir el inventario de cotizaciones
       },
       bars: 'horizontal',
       bar: {
-        groupWidth: '85%' 
+        groupWidth: '85%'
       },
       annotation: {
         textStyle: {
           fontSize: 0.4 // Ajusta el tamaño de la letra para las anotaciones
         },
-  
+
       }
     }
   }
@@ -125,18 +128,28 @@ variables globales para definir el inventario de cotizaciones
   /*================================
   variables para clientes destacados
   =================================*/
-  clienteElite="";
-  empleadoDestacado: any="";
-  mesTop="";
-  lstProveedores:Iproveedor[] = [];
+  clienteElite = "";
+  totalCLiente=0;
+  empleadoDestacado: any = "";
+  totalEmpleado = 0;
+  lstMesesTop:any;
+
+  lstProveedores: Iproveedor[] = [];
+
+  /*================================
+  variables para filtror
+  =================================*/
+  lstRepuestosMayoRotacion:any = [];
+  
+
 
   constructor(private productosService: ProductosService,
     private ventasService: VentasService,
     private clientesService: ClientesService,
     private empleadosService: EmpleadosService,
     private cotizacionesService: CotizacionesService,
-    private proveedoresService:ProveedoresService,
-    private comprasService:ComprasService
+    private proveedoresService: ProveedoresService,
+    private comprasService: ComprasService
   ) {
 
   }
@@ -178,11 +191,10 @@ Inventario de ventas
 
     this.loadVentas = true;
     this.chart.data = [];
-    this.barras.data=[];
-    this.clienteElite="";
-    this.empleadoDestacado="";
-    this.mesTop="";
-
+    this.barras.data = [];
+    this.clienteElite = "";
+    this.empleadoDestacado = "";
+    this.lstMesesTop=[];
     //TOTAL DE VENTAS
     this.ventasService.getData().subscribe(
       resp => {
@@ -232,22 +244,25 @@ Inventario de ventas
         //GRAFICO DE PRODUCTOS MAS VENDIDOS
 
         const top5Repuestos = this.obtenerProductosMasVendidos(res.data);
-        Object.keys(top5Repuestos).map((a:any) => {
-          const data = [ top5Repuestos[a].nombre, top5Repuestos[a].cantidadVendida,'eqtiqeu'];
+        Object.keys(top5Repuestos).map((a: any) => {
+          const data = [top5Repuestos[a].nombre, top5Repuestos[a].cantidadVendida, 'etiqueta'];
           this.barras.data.push(data);
         })
 
         //CLIENTE QUE MAS COMPRA
         //this.obtenerClienteQueMasCompra(res.data)
-        let idCli = this.obtenerClienteQueMasCompra(res.data).id;
-        this.clienteElite= this.lstClientes.find(e => e.cliId ===  idCli)?.cliNombres +' '+
-                                  this.lstClientes.find(e => e.cliId === idCli )?.cliApellidos ;
+        let cliente = this.obtenerClienteQueMasCompra(res.data);
+        this.totalCLiente = cliente.totalComprado;
+        this.clienteElite = this.lstClientes.find(e => e.cliId === cliente.id)?.cliNombres + ' ' +
+          this.lstClientes.find(e => e.cliId === cliente.id)?.cliApellidos;
         //MEJOR EMPLEADO
-        let idEmpl = this.obtenerEmpleadoQueMasVende(res.data).id;
-        this.empleadoDestacado= this.lstEmpleados.find(e => e.empId ===  idEmpl)?.empNombres +' '+
-                                  this.lstEmpleados.find(e => e.empId === idEmpl )?.emplApellidos ;
+        let empleado = this.obtenerEmpleadoQueMasVende(res.data);
+        this.totalEmpleado = empleado.totalVendido;
+        this.empleadoDestacado = this.lstEmpleados.find(e => e.empId === empleado.id)?.empNombres + ' ' +
+          this.lstEmpleados.find(e => e.empId === empleado.id)?.emplApellidos;
         //MES QUE MAS SE VENDE 
-        this.mesTop=this.obtenerMesEnQueSeVendeMas(res.data)
+        this.lstMesesTop = this.obtenerTop3MesesConVentas(res.data);
+        console.log("this.lstMesesTop: ", this.lstMesesTop);
 
       }
     )
@@ -265,7 +280,7 @@ Inventario de Empleados
     this.empleadosService.getData().subscribe(
       resp => {
         this.empleados = Object.keys(resp.data).length;
-        this.lstEmpleados= resp.data;
+        this.lstEmpleados = resp.data;
         this.loadEmpleados = false;
 
       }
@@ -301,25 +316,25 @@ Inventario de Empleados
 
         this.cotizacionesRecientes = Object.keys(resp.data).map(a => ({
 
-          cotId:   resp.data[a].cotId,
-          cotFecha:  resp.data[a].cotFecha,
+          cotId: resp.data[a].cotId,
+          cotFecha: resp.data[a].cotFecha,
           cotSubtotal: resp.data[a].cotSubtotal,
           cotDescuento: resp.data[a].cotDescuento,
           cotIva: resp.data[a].cotIva,
-          cotValorIva:  resp.data[a].cotValorIva,
+          cotValorIva: resp.data[a].cotValorIva,
           cotTotal: resp.data[a].cotTotal,
           cotEstado: resp.data[a].cotEstado,
-          cotIdEmpleado:  resp.data[a].cotIdEmpleado,
-          cotIdCliente:  resp.data[a].cotIdCliente,
-          cotIdMetPago:  resp.data[a].cotIdMetPago,
-          detalles:  resp.data[a].detalles,
+          cotIdEmpleado: resp.data[a].cotIdEmpleado,
+          cotIdCliente: resp.data[a].cotIdCliente,
+          cotIdMetPago: resp.data[a].cotIdMetPago,
+          detalles: resp.data[a].detalles,
           // cliNombres:this.lstClientes.find(n => n.cliId === resp.data[a].cotIdCliente )?.cliApellidos + ' '+
           //             this.lstClientes.find(n => n.cliId === resp.data[a].cotIdCliente )?.cliNombres 
-          cliNombres:this.lstClientes.find(n => n.cliId === resp.data[a].cotIdCliente )?.cliIdentificacion
+          cliNombres: this.lstClientes.find(n => n.cliId === resp.data[a].cotIdCliente)?.cliIdentificacion
 
-      } as Icotizacion))
+        } as Icotizacion))
 
-        this.cotizacionesRecientes = this.cotizacionesRecientes.slice(0,5)
+        this.cotizacionesRecientes = this.cotizacionesRecientes.slice(0, 5)
 
         this.loadCotizaciones = false;
       }
@@ -339,18 +354,18 @@ Inventario de Empleados
 
         this.ultimasCompras = Object.keys(resp.data).map(a => ({
 
-          comId:   resp.data[a].comId,
+          comId: resp.data[a].comId,
           comNumOrden: resp.data[a].comNumOrden,
           comFecha: resp.data[a].comFecha,
-          comProveedor : resp.data[a].comProveedor,
-          nombreProveedor:this.lstProveedores.find(p => p.proId ===  resp.data[a].comProveedor )?.proNombre,
-          comDescripcion : resp.data[a].comDescripcion,
+          comProveedor: resp.data[a].comProveedor,
+          nombreProveedor: this.lstProveedores.find(p => p.proId === resp.data[a].comProveedor)?.proNombre,
+          comDescripcion: resp.data[a].comDescripcion,
           comTotal: resp.data[a].comTotal,
-          detalles : resp.data[a].detalles
+          detalles: resp.data[a].detalles
 
         } as Icompra))
 
-        this.ultimasCompras =this.ultimasCompras.slice(0,5)
+        this.ultimasCompras = this.ultimasCompras.slice(0, 5)
         this.loadUltimasCompras = false;
 
       }
@@ -373,11 +388,14 @@ Inventario de Empleados
   Obtener los 5 productos + vendidos
   ********************************/
 
-  obtenerProductosMasVendidos(listaDeFacturas : any): any[] {
+  obtenerProductosMasVendidos(listaDeFacturas: any): any[] {
+
+    this.lstRepuestosMayoRotacion=[];
+
     // Organiza la lista de detalles de todas las facturas en un solo array
     const detallesDeTodasLasFacturas = listaDeFacturas
-      .map((factura:any) => factura.detalles)
-      .reduce((detallesAnteriores : any, detallesActuales: any) => detallesAnteriores.concat(detallesActuales), []);
+      .map((factura: any) => factura.detalles)
+      .reduce((detallesAnteriores: any, detallesActuales: any) => detallesAnteriores.concat(detallesActuales), []);
 
     // Agrupa los detalles por el id del producto y suma las cantidades vendidas
     const productosVendidos = detallesDeTodasLasFacturas.reduce((productos: any, detalle: any) => {
@@ -387,8 +405,10 @@ Inventario de Empleados
       } else {
         productos[idProducto] = {
           id: idProducto,
-          nombre:  detalle.codigoPils , // Reemplaza con el nombre real del producto
-          cantidadVendida: detalle.detCantidad
+          nombre: detalle.codigoPils, // Reemplaza con el nombre real del producto
+          cantidadVendida: detalle.detCantidad,
+          precio : detalle.detPrecio
+
         };
       }
       return productos;
@@ -398,7 +418,8 @@ Inventario de Empleados
     const productosVendidosArray = Object.values(productosVendidos);
 
     // Ordena el array por la cantidad vendida de manera descendente
-    productosVendidosArray.sort((a:any, b:any) => b.cantidadVendida - a.cantidadVendida);
+    productosVendidosArray.sort((a: any, b: any) => b.cantidadVendida - a.cantidadVendida);
+    this.lstRepuestosMayoRotacion = productosVendidosArray;
 
     // Obtiene los primeros 5 productos
     const primerosCincoProductos = productosVendidosArray.slice(0, 5);
@@ -410,9 +431,9 @@ Inventario de Empleados
   /*******************************
   Obtener el mejor cliente
   ********************************/
-  obtenerClienteQueMasCompra(listaDeFacturas:any): any {
+  obtenerClienteQueMasCompra(listaDeFacturas: any): any {
     // Agrupa las facturas por el id del cliente y suma los totales comprados
-    const clientesCompradores = listaDeFacturas.reduce((clientes:any, factura:any) => {
+    const clientesCompradores = listaDeFacturas.reduce((clientes: any, factura: any) => {
       const idCliente = factura.facIdCliente;
       if (clientes[idCliente]) {
         clientes[idCliente].totalComprado += factura.facTotal;
@@ -430,7 +451,7 @@ Inventario de Empleados
     const clientesCompradoresArray = Object.values(clientesCompradores);
 
     // Ordena el array por el total comprado de manera descendente
-    clientesCompradoresArray.sort((a:any, b:any) => b.totalComprado - a.totalComprado);
+    clientesCompradoresArray.sort((a: any, b: any) => b.totalComprado - a.totalComprado);
 
     // Obtiene el cliente que más compra
     const clienteQueMasCompra = clientesCompradoresArray.length > 0 ? clientesCompradoresArray[0] : null;
@@ -440,7 +461,7 @@ Inventario de Empleados
 
   obtenerEmpleadoQueMasVende(listaDeFacturas: any): any {
     // Agrupa las facturas por el id del empleado y suma los totales vendidos
-    const empleadosVendedores = listaDeFacturas.reduce((empleados:any, factura:any) => {
+    const empleadosVendedores = listaDeFacturas.reduce((empleados: any, factura: any) => {
       const idEmpleado = factura.facIdEmpleado;
       if (empleados[idEmpleado]) {
         empleados[idEmpleado].totalVendido += factura.facTotal;
@@ -458,7 +479,7 @@ Inventario de Empleados
     const empleadosVendedoresArray = Object.values(empleadosVendedores);
 
     // Ordena el array por el total vendido de manera descendente
-    empleadosVendedoresArray.sort((a:any, b:any) => b.totalVendido - a.totalVendido);
+    empleadosVendedoresArray.sort((a: any, b: any) => b.totalVendido - a.totalVendido);
 
     // Obtiene el empleado que más vende
     const empleadoQueMasVende = empleadosVendedoresArray.length > 0 ? empleadosVendedoresArray[0] : null;
@@ -467,7 +488,7 @@ Inventario de Empleados
   }
 
 
-  obtenerMesEnQueSeVendeMas(listaDeFacturas : any): string {
+  obtenerTop3MesesConVentas(listaDeFacturas: any): any[] {
     // Agrupa las facturas por el mes y suma los totales vendidos
     const ventasPorMes = listaDeFacturas.reduce((ventas: any, factura: any) => {
       const mes = new Date(factura.facFecha).toLocaleString('default', { month: 'long' });
@@ -478,13 +499,178 @@ Inventario de Empleados
       }
       return ventas;
     }, {});
-
-    // Encuentra el mes con mayores ventas
-    const mesConMasVentas = Object.keys(ventasPorMes).reduce((mesActual, mesSiguiente) =>
-      ventasPorMes[mesSiguiente] > ventasPorMes[mesActual] ? mesSiguiente : mesActual
+  
+    // Ordena los meses por ventas de mayor a menor
+    const mesesOrdenados = Object.keys(ventasPorMes).sort((mes1, mes2) =>
+      ventasPorMes[mes2] - ventasPorMes[mes1]
     );
-
-    return mesConMasVentas.toUpperCase();
+  
+    // Toma los tres primeros meses
+    const top3Meses = mesesOrdenados.slice(0, 3);
+  
+    // Crea una lista de objetos con el mes y las ventas
+    const resultado = top3Meses.map((mes) => ({
+      mes: mes,
+      ventas: ventasPorMes[mes],
+    }));
+  
+    return resultado;
   }
+
+
+  /***************************************
+  Función para obtener el reporte de  pedidos
+  ****************************************/
+  pedidoRepuestos() {
+
+    const doc = new jsPDF();
+    doc.setFontSize(18);
+    doc.text('Repuestos a Pedir', 20, 20);
+    this.productosService.getData().subscribe(
+      res => {
+        var pedido: any = [];
+        res.data.forEach((element: Iproducto) => {
+          if (element.proStockMinimo >= element.proStockTotal) {
+            const data = [element.proCodPils,
+                          element.proNumParte,
+                          element.proStockTotal,
+                          element.proPrecioCompra,
+                          element.proPvpEfectivo
+                      ];
+            pedido.push(data);
+          }
+        });
+        
+        autoTable(doc, {
+          head: [["Codigo", "Número Parte", "Stock Total", "Precio Compra", "Precio Venta"]],
+          body: [...pedido], 
+          startY: 30,
+        });
+        
+
+        const blob = doc.output('blob');
+        const url = URL.createObjectURL(blob);
+        window.open(url);
+        //doc.save('table.pdf');
+        
+
+      }
+    )
+  }
+
+
+  /*******************************
+  FUnción para repuestos de mayor rotacion
+  ********************************/
+repuestosMayorRotacion(){
+
+  console.log(this.lstRepuestosMayoRotacion)
+
+  var mayor: any = [];
+  this.lstRepuestosMayoRotacion.forEach((element : any) => {
+    const data =[element.id,
+                  element.nombre,
+                  element.cantidadVendida,
+                  element.precio
+    ]
+
+    mayor.push(data);
+    
+  });
+
+    const pdf = new jsPDF();
+    pdf.setFontSize(18);
+    pdf.text('Repuestos con Mayor Rotacion', 20, 20);
+
+        autoTable(pdf, {
+          head: [["Id", "Codigo Pils", "Cantidad Vendida", "Precio"]],
+          body: [...mayor], 
+          startY: 30,
+        });
+        
+
+        const blob = pdf.output('blob');
+        const url = URL.createObjectURL(blob);
+        window.open(url);
+
+}
+
+repuestosMayorStock(){
+
+  const doc = new jsPDF();
+  doc.setFontSize(18);
+  doc.text('Repuestos con mayor Stock', 20, 20);
+  this.productosService.getData().subscribe(
+    res => {
+      var pedido: any = [];
+      res.data.forEach((element: Iproducto) => {
+        var umbral_proporcion = 6.0 
+        const proporcion_stock = element.proStockTotal / element.proStockMinimo;
+        if ( proporcion_stock > umbral_proporcion ) {
+          const data = [element.proCodPils,
+                        element.proNumParte,
+                        element.proStockTotal,
+                        element.proStockMinimo,
+                        element.proPrecioCompra,
+                        element.proPvpEfectivo
+                    ];
+          pedido.push(data);
+        }
+      });
+      
+      autoTable(doc, {
+        head: [["Codigo", "Número Parte", "Stock Total","Stock Minimo" ,"Precio Compra", "Precio Venta"]],
+        body: [...pedido], 
+        startY: 30,
+      });
+      
+
+      const blob = doc.output('blob');
+      const url = URL.createObjectURL(blob);
+      window.open(url);
+      //doc.save('table.pdf');
+      
+
+    }
+  )
+}
+
+reporteGeneral(){
+
+  const doc = new jsPDF();
+  doc.setFontSize(18);
+  doc.text('Listado de  todos los repuestos', 20, 20);
+  this.productosService.getData().subscribe(
+    res => {
+      var general: any = [];
+      res.data.forEach((element: Iproducto) => {
+          const data = [element.proId,
+                        element.proCodPils,
+                        element.proNumParte,
+                        element.proNombre,
+                        element.proStockTotal,
+                        element.proPrecioCompra,
+                        element.proPvpEfectivo,
+                        element.proDescripcion
+                    ];
+          general.push(data);
+      });
+      
+      autoTable(doc, {
+        head: [["ID", "Cod. Pils", "Num. Parte","Nombre" ,"Stock Total", "Pre. Compra","Pre. Venta","Descripción"]],
+        body: [...general], 
+        startY: 30,
+      });
+      
+
+      const blob = doc.output('blob');
+      const url = URL.createObjectURL(blob);
+      window.open(url);
+      //doc.save('table.pdf');
+      
+
+    }
+  )
+}
 
 }
